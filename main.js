b2ea56aabc4f4a3af78191026de7d4d83491050a
masterofcolours <a24146288@gmail.com>
2026-08-24 21:47:14 +0330

    test5

--- main.js
+++
@@ -1,5 +0,0 @@
-import { Button } from "./Components/button/button.js";
-import { Header } from "./Components/header/header.js";
-
-window.customElements.define("header-item", Header)
-window.customElements.define("button-item", Button)
